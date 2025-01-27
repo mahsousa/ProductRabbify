92618089638a8cc8b33d5781c81867a7fd18ddf8
mahsousa <fatimssantos@gmail.com>
2025-01-27 17:11:13 -0300

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace ProductRabbify.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProdutoId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int Qtd { get; set; }
+
+    }
+}
